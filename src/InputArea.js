0222df8e397b1cd69f85a6f27d8f762850733af6
Rick Toews <rick.toews@gmail.com>
2021-02-22 23:54:40 -0800

    Some very fine adjustments.

--- src/InputArea.js
+++ src/InputArea.js
@@ -6,7 +6,8 @@ function InputArea(props) {
   const memTextWords = getWordList(memoryText);
   const inputEl = useRef(null);
 
-  const [state, setState] = useState({ textEl: null, entryStatus: [] });
+  const [wordState, setWordState] = useState({ textEl: null, entryStatus: [] });
+  const [verseState, setVerseState] = useState(true);
 
   useEffect(() => {
     inputEl.current.focus();
@@ -14,7 +15,7 @@ function InputArea(props) {
 
   function entryStatus() {
     var statusLine;
-    var wordArray = state.entryStatus.map((item, key) => {
+    var wordArray = wordState.entryStatus.map((item, key) => {
       var className = item.correct ? 'correct' : 'incorrect';
       return <span key={key} className={className}>{item.word} </span>;
     })
@@ -22,28 +23,30 @@ function InputArea(props) {
   }
 
   function evaluate() {
-    var { textEl } = state;
+    var { textEl } = wordState;
     var text = textEl && textEl.innerText || '';
     if (isWordDivider(text)) {
       var words = getWordList(text);
       var status = memCompare(words, memTextWords);
+      var hasIncorrect = status.filter(s => !s.correct).length > 0;
       if (isBlockFinished(status, memTextWords)) {
         getNextBlock();
       }
-      setState({ ...state, entryStatus: status });
+      setVerseState(!hasIncorrect);
+      setWordState({ ...wordState, entryStatus: status });
     }
   }
 
   function handleKey(e) {
     var textEl = e.target;
-    setState({ ...state, textEl });
+    setWordState({ ...wordState, textEl });
     evaluate();
   }
-
+console.log('verseState', verseState);
   return (
     <div>
-      <div className="entry-status">{entryStatus().map(w => w)}</div>
-      <div ref={inputEl} onKeyUp={handleKey} contentEditable="true" className="input-area" />
+      <div className="entry-status" style={{display: 'none'}}>{entryStatus().map(w => w)}</div>
+      <div ref={inputEl} onKeyUp={handleKey} className={'input-area ' + (!verseState ? 'incorrect' : '')} contentEditable="true" />
     </div>
   );
 }
